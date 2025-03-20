d4440046a6ecafeb93f2fe80028cdc0becda7d8e
dineshkumar-mb <dineshkumarmannibrundha@gmail.com>
2025-03-20 15:35:28 +0530

    cart,placeorder page created

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import { createContext } from "react";
 import { food_list } from "../assets/assets";
@@ -19,16 +19,24 @@ export const StoreContext  = createContext(null)
     const removeFromCart = (itemId) =>{
         setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
     }
-    useEffect (()=>{
-        console.log(cartItems);
-    },[cartItems])
+    const getTotalCartAmount = () =>{
+        let totalAmount = 0;
+        for (const item in cartItems) {
+            if(cartItems[item]>0){
+                let itemInfo = food_list.find((product)=>product._id===item);
+                totalAmount += itemInfo.price*cartItems[item];
+            }
+        }
+        return totalAmount;
+    }
 
     const contextValue ={
            food_list,
            cartItems ,
            setCartItems,
            addToCart,
-           removeFromCart
+           removeFromCart,
+           getTotalCartAmount
     }
 
    
